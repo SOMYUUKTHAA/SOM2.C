7dd1f998d60478e6da93759156dd777bc6af0497
SOMYUUKTHAA <30828746+SOMYUUKTHAA@users.noreply.github.com>
2018-02-10 21:35:17 +0530

    Update SOM2.C

--- SOM2.C
+++ SOM2.C
@@ -4,11 +4,13 @@ void main()
 int s;
 printf("Enter value");
 scanf("%d",&s);
-if(1<=s<=100000)
+if(s>=1 && s<=100000)
 {
 if(s%2==0)
 printf("THE ENTERED VALUE IS EVEN");
 else
 printf("THE ENTERED VALUE IS ODD");
 }
+else
+printf("\nvalue exceeds limit\n");
 }
